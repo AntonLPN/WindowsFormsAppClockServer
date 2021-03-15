548da35403fc7eeec5c63f5c83ce8b93e24b0afe
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-03-15 20:49:37 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsAppClockServer/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace WindowsFormsAppClockServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStartServer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonStartServer
+            // 
+            this.buttonStartServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStartServer.Location = new System.Drawing.Point(154, 103);
+            this.buttonStartServer.Name = "buttonStartServer";
+            this.buttonStartServer.Size = new System.Drawing.Size(118, 37);
+            this.buttonStartServer.TabIndex = 1;
+            this.buttonStartServer.Text = "Start Server";
+            this.buttonStartServer.UseVisualStyleBackColor = true;
+            this.buttonStartServer.Click += new System.EventHandler(this.buttonStartServer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 171);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(660, 267);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStartServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStartServer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsAppClockServer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppClockServer
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private async void Timer1_Tick(object sender, EventArgs e)
+        {
+            IPAddress address = IPAddress.Loopback;
+            IPEndPoint endPoint = new IPEndPoint(address, 11000);
+            Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            byte[] buff = Encoding.Unicode.GetBytes(DateTime.Now.ToString());
+            await sendSocket.SendToAsync(new ArraySegment<byte>(buff), SocketFlags.None, endPoint);
+            sendSocket.Shutdown(SocketShutdown.Send);
+            sendSocket.Close();
+
+        }
+     
+
+        private void buttonStartServer_Click(object sender, EventArgs e)
+        {
+             timer1.Start();
+
+            Task.Run(async () => {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+                IPAddress address = IPAddress.Loopback;
+                IPEndPoint endPoint = new IPEndPoint(address, 11000);
+                byte[] buff = new byte[1024];
+                EndPoint ep = new IPEndPoint(IPAddress.Any, 11000);
+                socket.Bind(endPoint);
+                do
+                {
+
+                    await socket.ReceiveFromAsync(new ArraySegment<byte>(buff), SocketFlags.None, ep).ContinueWith(t => {
+                        SocketReceiveFromResult res = t.Result;
+                        StringBuilder builder = new StringBuilder();
+                        builder.AppendLine($"{res.ReceivedBytes} байт получено из {res.RemoteEndPoint}");
+                        builder.AppendLine(Encoding.Unicode.GetString(buff, 0, res.ReceivedBytes));
+                        textBox1.BeginInvoke(new Action<string>(AddText), builder.ToString());
+                    });
+                }
+                while (true);
+            });
+
+
+
+
+        }
+
+        private void AddText(string str)
+        {
+            StringBuilder builder = new StringBuilder(textBox1.Text);
+            builder.AppendLine(str);
+            textBox1.Text = builder.ToString();
+        }
+
+       
+
+      
+    }
+}
